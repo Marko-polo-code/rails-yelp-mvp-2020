6186332f3dd3eb5feb3e6f6e8b444af1e1e9a154
Mark Muller <mark.muller.w@gmail.com>
2020-02-26 18:37:23 +0800

    styling erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+  get 'reviews/show'
+  get 'reviews/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get   'restaurants',      to: 'restaurants#index'
+
+  get   'restaurants/new',  to: 'restaurants#new', as: :new_restaurant
+  post  'restaurants',      to: 'restaurants#create', as: :create_restaurant
+  get   'restaurants/:id',  to: 'restaurants#show', as: :restaurant
+
+  get   'restaurants/:restaurant_id/reviews/new', to: 'reviews#new'
+  post  'restaurants/:restaurant_id/reviews',     to: 'reviews#create'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,27 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '847625238643',
+    category:     ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '847625238643',
+    category:     ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
